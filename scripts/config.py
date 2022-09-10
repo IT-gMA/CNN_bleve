fc690ca7ef6539c9c110971d559e638989ac9f06
IT-gMA <tiendatoan@gmail.com>
2022-09-10 23:21:51 +0800

    train with newly reworked 24x24 dataset

--- scripts/config.py
+++ scripts/config.py
@@ -2,15 +2,15 @@ import torch
 
 # Configure the training process
 LEARNING_RATE = 1e-3
-MIN_LEARNING_RATE = 0.0005598419915061102
+MIN_LEARNING_RATE = 0.00015
 WEIGHT_DECAY = 1e-06
-USE_DROP_OUT = True
+USE_DROP_OUT = False
 DROPOUT = 0.1994664332549059
-VARRY_LR = True
+VARRY_LR = False
 SCHEDULED = True
 MSE_REDUCTION = "mean"
 PATIENCE = 16
-BATCH_SIZE = 4096
+BATCH_SIZE = 1024
 VAL_BATCHSIZE = 512
 PRINT_TRAIN = False
 PRINT_VAL = False
@@ -18,9 +18,9 @@ PRINT_TEST = False
 
 # Image format
 RESCALE = True
-NUM_ROW = 30
-NUM_COLUMN = 30
-NUM_EPOCHS = 20000    # number of epochs to train for
+NUM_ROW = 120
+NUM_COLUMN = 120
+NUM_EPOCHS = 12000    # number of epochs to train for
 FILE_EXTENSION = ".png"
 
 MODEL_NAME = "resnet18"
@@ -31,8 +31,8 @@ print('Running on {}'.format(DEVICE))
 # Dataset directory: images and output
 OUTPUT_FILE = '../dataset/data/outputs/outputs.txt'
 IMG_DIR = '../dataset/data'
-SAVE_IMG_DIR = '../dataset_run/bleve_ordered_e1'
-SAVE_OUTPUT_DIR = '../dataset_run/bleve_ordered_e1/outputs/outputs.txt'
+SAVE_IMG_DIR = '../dataset_run/bleve_no_errv4'
+SAVE_OUTPUT_DIR = '../dataset_run/bleve_no_errv4/outputs/outputs.txt'
 
 # Dataset split
 SHUFFLE_TRAIN = True
@@ -46,22 +46,22 @@ ORDER_METHOD = 1
 NUM_WORKERS = 0
 
 # location to save model and plots
-SAVED_MODEL_DIR = "../saved_models/resnet18v2"
-SAVED_MODEL_NAME = "resnet18v2"
+SAVED_MODEL_DIR = "../saved_models/resnet18v4"
+SAVED_MODEL_NAME = "resnet18v4"
 SAVED_MODEL_FORMAT = ".pt"
 
 SAVE_PLOTS_EPOCH = 4 # save loss plots after these many epochs (the intervals of saving)
 SAVE_MODEL_EPOCH = 4 # save model after these many epochs
 
 # store model's run history
-LOG_DIR = "../running_logs/resnet18v2"
+LOG_DIR = "../running_logs/resnet18v4"
 
 
 # load model from
-LOAD_MODEL_LOCATION = "../saved_models/resnet18/resnet18best_model.pt"
+LOAD_MODEL_LOCATION = "../saved_models/resnet18v2/resnet18v2best_model.pt"
 
 # wandb running config
-WANDB_PROJECT_NAME = "CNN_bleve_resnet18v2"
+WANDB_PROJECT_NAME = "CNN_bleve_resnet18v4"
 
 
 # the size of tensor arrays being displayed
