5a589ff47901ed9dd255127c6abacc4f8e2e898b
IT-gMA <tiendatoan@gmail.com>
2022-10-07 21:24:48 +0800

    train on image size of 50x50

--- scripts/config.py
+++ scripts/config.py
@@ -4,30 +4,31 @@ import torch
 SEED_RANGE = 5
 
 # Configure the training process
-LEARNING_RATE = 1e-3
+LEARNING_RATE = 1e-3 #0.0033808570389529695
 MIN_LEARNING_RATE = 0.00015
-WEIGHT_DECAY = 1e-06
+WEIGHT_DECAY = 1.1067597759365375e-06
 USE_DROP_OUT = False
-DROPOUT = 0.1994664332549059
+DROPOUT = 0.2477008537587833
 VARRY_LR = False
 SCHEDULED = True
 MSE_REDUCTION = "mean"
 PATIENCE = 16
-BATCH_SIZE = 512
+BATCH_SIZE = 256
 VAL_BATCHSIZE = 256
 PRINT_TRAIN = False
 PRINT_VAL = False
 PRINT_TEST = False
-USE_PRETRAIN = False
 
 # Image format
 RESCALE = True
-NUM_ROW = 168
-NUM_COLUMN = 168
-NUM_EPOCHS = 1600    # number of epochs to train for
+NUM_ROW = 200
+NUM_COLUMN = 200
+NUM_EPOCHS = 1650    # number of epochs to train for
 FILE_EXTENSION = ".png"
 
+# Model config
 MODEL_NAME = "resnet18"
+USE_PRETRAIN = False
 DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('mps')
 EMPTY_CUDA_CACHE = False
 print('Running on {}'.format(DEVICE))
@@ -35,8 +36,8 @@ print('Running on {}'.format(DEVICE))
 # Dataset directory: images and output
 OUTPUT_FILE = '../dataset/data/outputs/outputs.txt'
 IMG_DIR = '../dataset/data'
-SAVE_IMG_DIR = '../dataset_run/bleve_no_errv4'
-SAVE_OUTPUT_DIR = '../dataset_run/bleve_no_errv4/outputs/outputs.txt'
+SAVE_IMG_DIR = '../dataset_run/bleve_50x50'
+SAVE_OUTPUT_DIR = '../dataset_run/bleve_50x50/outputs/outputs.txt'
 
 # Dataset split
 SHUFFLE_TRAIN = True
@@ -50,22 +51,22 @@ ORDER_METHOD = 1
 NUM_WORKERS = 0
 
 # location to save model and plots
-SAVED_MODEL_DIR = "../saved_models/resnet18v8"
-SAVED_MODEL_NAME = "resnet18v8"
+SAVED_MODEL_DIR = "../saved_models/resnet18v12"
+SAVED_MODEL_NAME = "resnet18v12"
 SAVED_MODEL_FORMAT = ".pt"
 
 SAVE_PLOTS_EPOCH = 4 # save loss plots after these many epochs (the intervals of saving)
 SAVE_MODEL_EPOCH = 4 # save model after these many epochs
 
 # store model's run history
-LOG_DIR = "../running_logs/resnet18v8"
+LOG_DIR = "../running_logs/resnet18v12"
 
 
 # load model from
 LOAD_MODEL_LOCATION = "../saved_models/resnet18v2/resnet18v2best_model.pt"
 
 # wandb running config
-WANDB_PROJECT_NAME = "CNN_bleve_resnet18v8"
+WANDB_PROJECT_NAME = "CNN_bleve_resnet18v12"
 
 
 # the size of tensor arrays being displayed
@@ -73,4 +74,4 @@ NP_FULL_SIZE = False
 
 # nth epoch at which a checkpoint is saved
 SAVED_EPOCH = 100
-SAVED_AFTER = 799
+SAVED_AFTER = 999
