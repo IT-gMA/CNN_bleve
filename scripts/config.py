28cdf896827bf50a4dbc51ad0b5889aa05205c16
IT-gMA <tiendatoan@gmail.com>
2022-10-10 13:57:24 +0800

    train with augmentations enabled

--- scripts/config.py
+++ scripts/config.py
@@ -10,7 +10,7 @@ WEIGHT_DECAY = 1.1067597759365375e-06
 USE_DROP_OUT = False
 DROPOUT = 0.2477008537587833
 VARRY_LR = False
-SCHEDULED = True
+SCHEDULED = False
 MSE_REDUCTION = "mean"
 PATIENCE = 16
 BATCH_SIZE = 256
@@ -25,10 +25,11 @@ NUM_ROW = 200
 NUM_COLUMN = 200
 NUM_EPOCHS = 1650    # number of epochs to train for
 FILE_EXTENSION = ".png"
+TRAIN_AUG = True
 
 # Model config
-MODEL_NAME = "resnet18"
-USE_PRETRAIN = False
+MODEL_NAME = "resnet34"
+USE_PRETRAIN = True
 DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('mps')
 EMPTY_CUDA_CACHE = False
 print('Running on {}'.format(DEVICE))
@@ -36,8 +37,8 @@ print('Running on {}'.format(DEVICE))
 # Dataset directory: images and output
 OUTPUT_FILE = '../dataset/data/outputs/outputs.txt'
 IMG_DIR = '../dataset/data'
-SAVE_IMG_DIR = '../dataset_run/bleve_50x50'
-SAVE_OUTPUT_DIR = '../dataset_run/bleve_50x50/outputs/outputs.txt'
+SAVE_IMG_DIR = '../dataset_run/bleve_orderd_e0'
+SAVE_OUTPUT_DIR = '../dataset_run/bleve_orderd_e0/outputs/outputs.txt'
 
 # Dataset split
 SHUFFLE_TRAIN = True
@@ -51,22 +52,22 @@ ORDER_METHOD = 1
 NUM_WORKERS = 0
 
 # location to save model and plots
-SAVED_MODEL_DIR = "../saved_models/resnet18v12"
-SAVED_MODEL_NAME = "resnet18v12"
+SAVED_MODEL_DIR = "../saved_models/resnet34v3"
+SAVED_MODEL_NAME = "resnet34v3"
 SAVED_MODEL_FORMAT = ".pt"
 
 SAVE_PLOTS_EPOCH = 4 # save loss plots after these many epochs (the intervals of saving)
 SAVE_MODEL_EPOCH = 4 # save model after these many epochs
 
 # store model's run history
-LOG_DIR = "../running_logs/resnet18v12"
+LOG_DIR = "../running_logs/resnet34v3"
 
 
 # load model from
 LOAD_MODEL_LOCATION = "../saved_models/resnet18v2/resnet18v2best_model.pt"
 
 # wandb running config
-WANDB_PROJECT_NAME = "CNN_bleve_resnet18v12"
+WANDB_PROJECT_NAME = "CNN_bleve_resnet34v3"
 
 
 # the size of tensor arrays being displayed
@@ -75,3 +76,6 @@ NP_FULL_SIZE = False
 # nth epoch at which a checkpoint is saved
 SAVED_EPOCH = 100
 SAVED_AFTER = 999
+
+# Progress bar
+PROGRESS_SLEEP_TIME = 0.0002
