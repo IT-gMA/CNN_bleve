60a63a0ec4f8396b8a2c6a22c9a4cf711835b8fc
IT-gMA <tiendatoan@gmail.com>
2022-11-24 22:29:14 +0800

    train config for gcolab

--- scripts/config.py
+++ scripts/config.py
@@ -13,7 +13,7 @@ VARRY_LR = False
 SCHEDULED = True
 MSE_REDUCTION = "mean"
 PATIENCE = 16
-BATCH_SIZE = 256
+BATCH_SIZE = 1024
 VAL_BATCHSIZE = 256
 PRINT_TRAIN = False
 PRINT_VAL = False
@@ -21,15 +21,15 @@ PRINT_TEST = False
 
 # Image format
 RESCALE = True
-NUM_ROW = 240
-NUM_COLUMN = 240
+NUM_ROW = 250
+NUM_COLUMN = 250
 NUM_EPOCHS = 1000    # number of epochs to train for
 FILE_EXTENSION = ".png"
 TRAIN_AUG = False
 
 # Model config
 RESUME = False
-MODEL_NAME = "resnet18"
+MODEL_NAME = "resnet50"
 USE_PRETRAIN = True
 DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('mps')
 EMPTY_CUDA_CACHE = False
@@ -39,8 +39,8 @@ VALIDATION_EPOCH = 5
 # Dataset directory: images and output
 OUTPUT_FILE = '../dataset/data/outputs/outputs.txt'
 IMG_DIR = '../dataset/data'
-SAVE_IMG_DIR = '../temp_dataset/DI_bleve_40x40_large_frame'
-SAVE_OUTPUT_DIR = '../temp_dataset/DI_bleve_40x40_large_frame/outputs/outputs.txt'
+SAVE_IMG_DIR = '../dataset_run/bleve_50x50'
+SAVE_OUTPUT_DIR = '../dataset_run/bleve_50x50/outputs/outputs.txt'
 
 # Dataset split
 SHUFFLE_TRAIN = True
@@ -54,22 +54,22 @@ ORDER_METHOD = 1
 NUM_WORKERS = 0
 
 # location to save model and plots
-SAVED_MODEL_DIR = "../saved_models/resnet18v15"
-SAVED_MODEL_NAME = "resnet18v15"
+SAVED_MODEL_DIR = "../saved_models/resnet50v4"
+SAVED_MODEL_NAME = "resnet50v4"
 SAVED_MODEL_FORMAT = ".pt"
 
 SAVE_PLOTS_EPOCH = 4 # save loss plots after these many epochs (the intervals of saving)
 SAVE_MODEL_EPOCH = 4 # save model after these many epochs
 
 # store model's run history
-LOG_DIR = "../running_logs/resnet18v15"
+LOG_DIR = "../running_logs/resnet50v4"
 
 
 # load model from
 LOAD_MODEL_LOCATION = "../saved_models/resnet18v10/seed_3/resnet18v10_801_model.pt"
 
 # wandb running config
-WANDB_PROJECT_NAME = "CNN_bleve_resnet18v15"
+WANDB_PROJECT_NAME = "CNN_bleve_resnet50v4"
 
 
 # the size of tensor arrays being displayed
