2e91af584e7287288a3a17d0363e01073dc9f1e0
IT-gMA <tiendatoan@gmail.com>
2022-11-24 20:53:09 +0800

    train on DeepInsight dataset

--- scripts/config.py
+++ scripts/config.py
@@ -1,7 +1,7 @@
 import torch
 
 # Random shuffle of dataset
-SEED_RANGE = 5
+SEED_RANGE = 4
 
 # Configure the training process
 LEARNING_RATE = 1e-3 #0.0033808570389529695
@@ -25,20 +25,22 @@ NUM_ROW = 240
 NUM_COLUMN = 240
 NUM_EPOCHS = 1000    # number of epochs to train for
 FILE_EXTENSION = ".png"
-TRAIN_AUG = True
+TRAIN_AUG = False
 
 # Model config
+RESUME = False
 MODEL_NAME = "resnet18"
 USE_PRETRAIN = True
 DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('mps')
 EMPTY_CUDA_CACHE = False
 print('Running on {}'.format(DEVICE))
+VALIDATION_EPOCH = 5
 
 # Dataset directory: images and output
 OUTPUT_FILE = '../dataset/data/outputs/outputs.txt'
 IMG_DIR = '../dataset/data'
-SAVE_IMG_DIR = '../dataset_run/bleve_40x40'
-SAVE_OUTPUT_DIR = '../dataset_run/bleve_40x40/outputs/outputs.txt'
+SAVE_IMG_DIR = '../temp_dataset/DI_bleve_40x40_large_frame'
+SAVE_OUTPUT_DIR = '../temp_dataset/DI_bleve_40x40_large_frame/outputs/outputs.txt'
 
 # Dataset split
 SHUFFLE_TRAIN = True
@@ -52,29 +54,29 @@ ORDER_METHOD = 1
 NUM_WORKERS = 0
 
 # location to save model and plots
-SAVED_MODEL_DIR = "../saved_models/resnet18v11"
-SAVED_MODEL_NAME = "resnet18v11"
+SAVED_MODEL_DIR = "../saved_models/resnet18v15"
+SAVED_MODEL_NAME = "resnet18v15"
 SAVED_MODEL_FORMAT = ".pt"
 
 SAVE_PLOTS_EPOCH = 4 # save loss plots after these many epochs (the intervals of saving)
 SAVE_MODEL_EPOCH = 4 # save model after these many epochs
 
 # store model's run history
-LOG_DIR = "../running_logs/resnet18v11"
+LOG_DIR = "../running_logs/resnet18v15"
 
 
 # load model from
-LOAD_MODEL_LOCATION = "../saved_models/resnet18v2/resnet18v2best_model.pt"
+LOAD_MODEL_LOCATION = "../saved_models/resnet18v10/seed_3/resnet18v10_801_model.pt"
 
 # wandb running config
-WANDB_PROJECT_NAME = "CNN_bleve_resnet18v11"
+WANDB_PROJECT_NAME = "CNN_bleve_resnet18v15"
 
 
 # the size of tensor arrays being displayed
 NP_FULL_SIZE = False
 
 # nth epoch at which a checkpoint is saved
-SAVED_EPOCH = 100
+SAVED_EPOCH = 200
 SAVED_AFTER = 499
 
 # Progress bar
